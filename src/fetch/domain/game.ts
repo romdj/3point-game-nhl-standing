c02c7f09e74b1eb0e801de23ae3e794732941483
Romain Lussier <romdj_123@hotmail.com>
2020-03-26 00:15:25 +0100

    refactoring: move functions outside class

--- src/fetch/domain/game.ts
+++ src/fetch/domain/game.ts
@@ -9,47 +9,47 @@ export class Game {
 
   constructor(gameNHLSchedule: any) {
     this._id = gameNHLSchedule.gamePk;
-    this.winnerId = this.getWinner(gameNHLSchedule);
-    this.loserId = this.getLoser(gameNHLSchedule);
+    this.winnerId = getWinner(gameNHLSchedule);
+    this.loserId = getLoser(gameNHLSchedule);
     this.winType = WinType.UNDEFINED;
-    if (this.isRegulation(gameNHLSchedule)) this.winType = WinType.REGULATION;
-    if (this.isOvertime(gameNHLSchedule)) this.winType = WinType.OVERTIME;
-    if (this.isShootout(gameNHLSchedule)) this.winType = WinType.SHOOTOUT;
+    if (isRegulation(gameNHLSchedule)) this.winType = WinType.REGULATION;
+    if (isOvertime(gameNHLSchedule)) this.winType = WinType.OVERTIME;
+    if (isShootout(gameNHLSchedule)) this.winType = WinType.SHOOTOUT;
   }
+}
 
-  private isComplete = (gameNHLSchedule: any): boolean => {
-    return gameNHLSchedule.linescore.currentPeriodTimeRemaining === 'Final';
-  }
+const isComplete = (gameNHLSchedule: any): boolean => {
+  return gameNHLSchedule.linescore.currentPeriodTimeRemaining === 'Final';
+}
 
-  private isRegulation = (gameNHLSchedule: any): boolean => {
-    return gameNHLSchedule.linescore.currentPeriodOrdinal === '3rd' && this.isComplete(gameNHLSchedule);
-  }
+const isRegulation = (gameNHLSchedule: any): boolean => {
+  return gameNHLSchedule.linescore.currentPeriodOrdinal === '3rd' && isComplete(gameNHLSchedule);
+}
 
-  private isOvertime = (gameNHLSchedule: any): boolean => {
-    return gameNHLSchedule.linescore.currentPeriodOrdinal === 'OT' && this.isComplete(gameNHLSchedule);
-  }
+const isOvertime = (gameNHLSchedule: any): boolean => {
+  return gameNHLSchedule.linescore.currentPeriodOrdinal === 'OT' && isComplete(gameNHLSchedule);
+}
 
-  private isShootout = (gameNHLSchedule: any): boolean => {
-    return gameNHLSchedule.linescore.currentPeriodOrdinal === 'SO' && this.isComplete(gameNHLSchedule);
-  }
+const isShootout = (gameNHLSchedule: any): boolean => {
+  return gameNHLSchedule.linescore.currentPeriodOrdinal === 'SO' && isComplete(gameNHLSchedule);
+}
 
-  private isHomeWin = (gameNHLSchedule: any): boolean => {
-    return gameNHLSchedule.linescore.teams.home.goals > gameNHLSchedule.linescore.teams.away.goals;
-  }
+const isHomeWin = (gameNHLSchedule: any): boolean => {
+  return gameNHLSchedule.linescore.teams.home.goals > gameNHLSchedule.linescore.teams.away.goals;
+}
 
-  private getWinner = (gameNHLSchedule: any): string => {
-    if (this.isHomeWin(gameNHLSchedule))
-      return gameNHLSchedule.linescore.teams.home.team.name;
-    return gameNHLSchedule.linescore.teams.away.team.name;
-    // return gameNHLSchedule.linescore.teams.home.team.id;
-    // return gameNHLSchedule.linescore.teams.away.team.id;
-  }
-
-  private getLoser = (gameNHLSchedule: any): string => {
-    if (this.isHomeWin(gameNHLSchedule))
-      return gameNHLSchedule.linescore.teams.away.team.name;
+const getWinner = (gameNHLSchedule: any): string => {
+  if (isHomeWin(gameNHLSchedule))
     return gameNHLSchedule.linescore.teams.home.team.name;
-    // return gameNHLSchedule.linescore.teams.away.team.id;
-    // return gameNHLSchedule.linescore.teams.home.team.id;
-  }
+  return gameNHLSchedule.linescore.teams.away.team.name;
+  // return gameNHLSchedule.linescore.teams.home.team.id;
+  // return gameNHLSchedule.linescore.teams.away.team.id;
+}
+
+const getLoser = (gameNHLSchedule: any): string => {
+  if (isHomeWin(gameNHLSchedule))
+    return gameNHLSchedule.linescore.teams.away.team.name;
+  return gameNHLSchedule.linescore.teams.home.team.name;
+  // return gameNHLSchedule.linescore.teams.away.team.id;
+  // return gameNHLSchedule.linescore.teams.home.team.id;
 }
