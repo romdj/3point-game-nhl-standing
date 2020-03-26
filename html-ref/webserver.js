bf86c6e39b218787034f564580c16468bcec7fb8
Romain Lussier <romdj_123@hotmail.com>
2020-03-26 21:04:21 +0100

    removing noise from repository

--- html-ref/webserver.js
+++
@@ -1,7 +0,0 @@
-const { readdirSync, readFileSync } = require("fs");
-const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.static('public'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
