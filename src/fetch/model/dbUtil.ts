7c4bdb9b38bfb0016b1d397aaa1ca8f299c6b91a
Romain Lussier <romdj_123@hotmail.com>
2020-03-28 00:33:49 +0100

    more dbUtils changes

--- src/fetch/model/dbUtil.ts
+++ src/fetch/model/dbUtil.ts
@@ -1,8 +1,11 @@
 import * as AWS from 'aws-sdk';
 
-const elasticache = new AWS.ElastiCache();
+const elasticache = new AWS.ElastiCache({apiVersion: '2015-02-02'});
 
-export async function uploadElasticache(data: any) {
+export async function uploadElasticache(toOffload: any) {
+    const params = {
+
+    };
     await elasticache.addTagsToResource(params, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
