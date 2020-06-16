f80cd80a05730b1d22e7d32f991c1dae3998aecb
Romain Lussier <romdj_123@hotmail.com>
2020-06-16 10:54:19 +0200

    removing hidden/unused unit tests

--- .test/unit/fetch/domain/leagueRecord.ts
+++
@@ -1,8 +0,0 @@
-import 'mocha';
-import { expect } from 'chai';
-
-import { LeagueRecord } from "../../../../src/fetch/domain/leagueRecord";
-
-describe('LeagueRecord', () => {
-    it('Creating Valid Record for NHL Team', () => { });
-});
