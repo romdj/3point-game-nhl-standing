e9d7e8020861157568bce986b62a5eb96cdd80d7
Romain Lussier <romdj_123@hotmail.com>
2020-06-17 20:33:55 +0200

    adding core elements for fetch unit tests

--- src/fetch/fetch.spec.ts
+++ src/fetch/fetch.spec.ts
@@ -1,9 +1,26 @@
 import 'mocha';
 import { expect } from 'chai';
-import { fetchNHLSchedule } from './fetch';
+import { fetchNHLSchedule, getSeasonStanding } from './fetch';
 
 describe.only('Fetch/main', () => {
-    it('should get full schedule for something', () => {
-        console.log(fetchNHLSchedule());
+    describe('Getting schedule', () => {
+        it('should get full schedule for current season (not provided)', async () => {
+            const schedule = await fetchNHLSchedule();
+            console.log(schedule);
+        });
+        it('should get full schedule for a given season', async () => {
+            const schedule = await fetchNHLSchedule('20172018');
+            console.log(schedule);
+        });
+    });
+    describe('Getting API standings', () => {
+        it('should get full standings for current season (not provided)', async () => {
+            const standings = await getSeasonStanding()
+            console.log(standings);
+        });
+        it('should get full standings for a given season', async () => {
+            const standings = await getSeasonStanding('20172018')
+            console.log(standings);
+        });
     });
 });
