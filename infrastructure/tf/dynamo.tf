7110310628d6c84aa235e13d32364caf307435c3
romdj <romdj_123@hotmail.com>
2022-11-14 16:19:49 +0100

    Attempt to make TF work

--- infrastructure/tf/dynamo.tf
+++ infrastructure/tf/dynamo.tf
@@ -1,8 +1,7 @@
-resource "aws_dynamodb_table" "nhl-standings" {
-  dynamodb_table_name           = "<tableName>"
-  billing_mode        = "PAY_PER_REQUEST"
+resource "aws_dynamodb_table" "nhl_match_points_table" {
+  dynamodb_table_name           = "<env>"
+  billing_mode                  = "PAY_PER_REQUEST"
   hash_key                      = "gamePk"
-
   attribute {
     name = "gamePk"
     type = "S"
