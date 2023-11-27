50fb1ac4d63128ecc3f983f3f22b93b24fa0ec89
Romain <romdj_123@hotmail.com>
2023-11-27 18:52:19 +0100

    added some scripts to get started

--- src/graphql/js-sandbox/test.js
+++ src/graphql/js-sandbox/test.js
@@ -1,7 +1,7 @@
-const { RESTDataSource } = require('@apollo/datasource-rest');
+const { RESTDataSource } = require("@apollo/datasource-rest");
 
 class NHLAPI extends RESTDataSource {
- baseURL = "https://api-web.nhle.com/v1";
+  baseURL = "https://api-web.nhle.com/v1";
 
   async getScheduleFrom(dateFrom) {
     const response = await this.get(`${this.baseURL}/schedule/${dateFrom}`);
@@ -10,16 +10,20 @@ class NHLAPI extends RESTDataSource {
 
   async getScheduleDay(dateDay) {
     const gameDaysCollection = await this.getScheduleFrom(dateDay);
-    return gameDaysCollection
-      .filter(gameDay => gameDay.date === dateDay)
-      .map(gameDay=> gameDay.games)
+    return gameDaysCollection.filter((gameDay) => gameDay.date === dateDay).map((gameDay) => gameDay.games);
+  }
+
+  async getNhlStandings() {
+    const response = await this.get(`${this.baseURL}/standings-season`);
+    return response;
   }
 }
 
 (async () => {
   const nhlApi = new NHLAPI();
-  const dateFrom = await nhlApi.getScheduleFrom("2023-11-26");
-  const gamesOn = await nhlApi.getScheduleDay("2023-11-26");
-  console.log(`dateFrom: ${JSON.stringify(dateFrom)}`);
-  console.log(`gamesOn: ${JSON.stringify(gamesOn)}`);
+  // const dateFrom = await nhlApi.getScheduleFrom("2023-11-26");
+  // const gamesOn = await nhlApi.getScheduleDay("2023-11-26");
+  // console.log(`dateFrom: ${JSON.stringify(dateFrom)}`);
+  // console.log(`gamesOn: ${JSON.stringify(gamesOn)}`);
+  console.log(`standings: ${JSON.stringify(await nhlApi.getNhlStandings(), null, 2)}`);
 })();
