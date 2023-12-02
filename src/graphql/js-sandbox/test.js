44a8b6bc8dd66ade1c5187deefed0a3298aa4873
Romain <romdj_123@hotmail.com>
2023-12-02 11:49:24 +0100

    clean previous code and test - create svelte app -

--- src/graphql/js-sandbox/test.js
+++ src/graphql/js-sandbox/test.js
@@ -1,7 +1,8 @@
-const { RESTDataSource } = require("@apollo/datasource-rest");
+const { writeFileSync } = require('fs');
+const { RESTDataSource } = require('@apollo/datasource-rest');
 
 class NHLAPI extends RESTDataSource {
-  baseURL = "https://api-web.nhle.com/v1";
+  baseURL = 'https://api-web.nhle.com/v1';
 
   async getScheduleFrom(dateFrom) {
     const response = await this.get(`${this.baseURL}/schedule/${dateFrom}`);
@@ -13,17 +14,31 @@ class NHLAPI extends RESTDataSource {
     return gameDaysCollection.filter((gameDay) => gameDay.date === dateDay).map((gameDay) => gameDay.games);
   }
 
-  async getNhlStandings() {
+  async getAllNhlStandingsReferences() {
     const response = await this.get(`${this.baseURL}/standings-season`);
     return response;
+  }
+
+  async getStandingsForDate(standingsDate) {
+    console.log(`querying: ${this.baseURL}/standings/${standingsDate}`);
+    const response = await this.get(`${this.baseURL}/standings/${standingsDate}`);
+    return response;
+  }
+
+  async formatNhlStandings(standings) {
+    // const response = await this.get(`${this.baseURL}/v1/standings/${standingsDate}`);
+    // return response;
   }
 }
 
 (async () => {
   const nhlApi = new NHLAPI();
-  // const dateFrom = await nhlApi.getScheduleFrom("2023-11-26");
-  // const gamesOn = await nhlApi.getScheduleDay("2023-11-26");
+  // const dateFrom = await nhlApi.getScheduleFrom('2023-11-26');
+  // const gamesOn = await nhlApi.getScheduleDay('2023-11-26');
   // console.log(`dateFrom: ${JSON.stringify(dateFrom)}`);
   // console.log(`gamesOn: ${JSON.stringify(gamesOn)}`);
-  console.log(`standings: ${JSON.stringify(await nhlApi.getNhlStandings(), null, 2)}`);
+  const nhlStandings = await nhlApi.getStandingsForDate('2023-12-02');
+  // console.log(`standings: ${JSON.stringify(await nhlApi.getNhlStandings(), null, 2)}`);
+  writeFileSync('standings.json', JSON.stringify(nhlStandings, null, 2), 'utf8');
+  // console.log(`standings: ${nhlStandings.}`);
 })();
