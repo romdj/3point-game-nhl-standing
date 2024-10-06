cfea4583c758fb07a1465fee86112c660ea61ab8
Romain Lussier <romdj_123@hotmail.com>
2024-10-06 05:57:57 +0200

    Added svelte core structure - wip

--- front-end/svelte/international-nhl-standings/src/routes/+page.svelte
+++ front-end/svelte/international-nhl-standings/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Table from "../components/Table.svelte";
+  import { onMount } from "svelte";
+  import { fetchStandings } from "../api/standingsAPI";
+  import { standingsStore } from "../stores/standingsStore";
+
+  onMount(async () => {
+    const standings = await fetchStandings();
+    standingsStore.set(standings);
+  });
+</script>
+
+<main>
+  <h1>NHL Standings</h1>
+  <Table />
+</main>
