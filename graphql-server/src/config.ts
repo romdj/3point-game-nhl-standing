d924fb9b939777da9a351fe7b5bb111fee9842a4
Romain Lussier <romdj_123@hotmail.com>
2024-11-11 07:50:45 +0100

    graphql: added CORS configuration (to be adjusted upon deploying)

--- graphql-server/src/config.ts
+++ graphql-server/src/config.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export const config = {
   port: 4000,
   graphql_playground: process.env.GRAPHQL_PLAYGROUND === 'true' || true,
+  cors_origin: process.env.CORS_ORIGIN || 'http://localhost:5173', // Add CORS origin configuration
 };
